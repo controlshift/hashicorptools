d8c5a181f5c08561f1626b0f69287acb1061c7e4
Nathan Woodhull <woodhull@gmail.com>
2015-10-09 17:10:31 -0400

    include the tag name in the test machine name.

--- lib/packer.rb
+++ lib/packer.rb
@@ -46,7 +46,7 @@ class Packer < Thor
       instance_type: "t2.micro")
 
     ec2.create_tags( resources: run_instances_resp.instances.collect{|i| i.instance_id },
-        tags: [ {key: 'Name', value: "packer test boot"}, {key: 'environment', value: 'packer-development'}, {key: 'temporary', value: 'kill me'}])
+        tags: [ {key: 'Name', value: "packer test boot #{tag_name}"}, {key: 'environment', value: 'packer-development'}, {key: 'temporary', value: 'kill me'}])
 
     require 'pry-byebug'
     binding.pry
