5d9f38cbe635faff696bf8b303dad3237e1de58b
Nathan Woodhull <woodhull@gmail.com>
2015-10-16 16:46:58 -0400

    improve command line ux.

--- lib/hashicorptools/host.rb
+++ lib/hashicorptools/host.rb
@@ -21,37 +21,56 @@ module Hashicorptools
     option :environment, required: false
     option :role, required: false
     option :name, required: false
-    def ssh
+    def ssh(role = '')
       ec2 = Aws::EC2::Client.new(region: 'us-east-1')
 
-      resp = ec2.describe_instances(filters: filters) 
-      dns = resp.reservations.first.instances.first.public_dns_name
+      resp = ec2.describe_instances(filters: filters(role))
+      instance = resp.reservations.first.instances.first
+
+      dns = if instance.public_dns_name.present?
+        instance.public_dns_name
+      else
+        instance.private_dns_name
+      end
 
       exec "ssh #{ssh_user_fragment}#{dns}"
     end
 
-
     private
+
+    def application_environment
+      if options[:environment].present?
+        options[:environment]
+      elsif ENV['CHANGESPROUT_APP_ENVIRONMENT'].present?
+        ENV['CHANGESPROUT_APP_ENVIRONMENT']
+      else
+        'staging'
+      end
+    end
 
     def ssh_user_fragment
       ENV['AWS_SSH_USERNAME'].present? ? "#{ENV['AWS_SSH_USERNAME']}@" : ''
     end
 
-    def filters
+    def filters(role = '')
       filters = []
 
       filters << {name: 'instance-state-name', values: ['running']}
 
-      if options[:environment].present?
-        filters << {name: 'tag:environment', values: [ options[:environment] ]}
+      if application_environment.present?
+        filters << {name: 'tag:environment', values: [ application_environment ]}
       end
 
       if options[:name].present?
         filters << {name: 'tag:Name', values: [ options[:name] ]}
       end
 
-      if options[:role].present?
-        filters << {name: 'tag:role', values: [ options[:role] ]}
+      if role.blank?
+        role = options[:role].present?
+      end
+
+      if role.present?
+        filters << {name: 'tag:role', values: [ role ]}
       end
 
       filters
