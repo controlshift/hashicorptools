fa4cb86810f4bfac607781659c9c3ee270f8dcb8
Jacinda Moore <alynna.kasmira@gmail.com>
2015-11-02 18:06:37 -0500

    add utility functions for looking up vpc & gateway by vpc name

--- lib/hashicorptools/ec2_utilities.rb
+++ lib/hashicorptools/ec2_utilities.rb
@@ -12,6 +12,16 @@ module Hashicorptools
       ec2 = AWS::EC2.new
     end
 
+    def vpc_with_name(name)
+      vpcs = ec2.client.describe_vpcs({filters: [{name: 'tag:Name', values: [name]}]}).vpc_set
+      vpcs.first
+    end
+
+    def internet_gateway_for_vpc(vpc_id)
+      igs = ec2.client.describe_internet_gateways({filters: [{name: 'attachment.vpc-id', values: [vpc_id]}]}).internet_gateway_set
+      igs.first
+    end
+
     def image_created_at(image, tag= tag_name)
       if image
         image.tags.to_h[tag].to_i
